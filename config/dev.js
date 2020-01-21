e14883325630a1c01b7fbc0ced20f6f8985fa9e9
melmarcondes <mel.marcondes@gmail.com>
2020-01-21 16:37:21 +0100

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://mbeff:qwert12345@cluster0-shard-00-00-3iu7s.gcp.mongodb.net:27017,cluster0-shard-00-01-3iu7s.gcp.mongodb.net:27017,cluster0-shard-00-02-3iu7s.gcp.mongodb.net:27017/blog_dev?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
